07aaa4a1a730ec6529c4537fc52cfda319e43b44
thuyainsoe <thuyainsoe163361@gmail.com>
2024-07-13 23:58:49 +0700

    feat: nav

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
 const Navbar = ({ dark, clickToggle }) => {
+  const [showNav, setShowNav] = useState(false);
   return (
     <div className="container-md py-[30px] h-[100px] flex items-center justify-between w-full bg-white dark:bg-[#121212] sticky top-0 z-[1]">
       <div className="w-fit text-white py-[5px] rounded-lg px-[10px] font-bold text-[14px] text-nowrap background-linear">
         {"< TYS />"}
       </div>
-      <div className="flex items-center gap-[30px] justify-between">
+      <div className="items-center gap-[30px] justify-between hidden lg:flex">
         <ul className="flex items-center gap-[20px] text-[14px] font-bold text-[#282828] dark:text-white">
           <NavLink
             to="/"
@@ -118,6 +120,152 @@ const Navbar = ({ dark, clickToggle }) => {
             <span className="ball bg-white dark:bg-[#111]"></span>
           </label>
         </div>
+      </div>
+
+      <svg
+        className="w-[20px] lg:hidden fill-black dark:fill-white"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+        onClick={() => {
+          setShowNav(true);
+        }}
+      >
+        <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+      </svg>
+
+      <div
+        className={`lg:hidden transition-3s fixed top-0 z-[100] left-[-100%] w-full h-full bg-white dark:bg-[#121212] ${
+          showNav ? "!left-0" : ""
+        }`}
+      >
+        <div className="flex items-center justify-between p-[20px] py-[30px] h-[100px]">
+          <div className="w-fit text-white py-[5px] rounded-lg px-[10px] font-bold text-[14px] text-nowrap background-linear">
+            {"< TYS />"}
+          </div>
+          <svg
+            className="w-[20px]"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 384 512"
+            onClick={() => {
+              setShowNav(false);
+            }}
+          >
+            <path
+              className="fill-black dark:fill-white"
+              d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
+            />
+          </svg>
+        </div>
+        <div className="ml-[20px]">
+          <input
+            type="checkbox"
+            className="checkbox"
+            id="checkbox"
+            value={dark}
+            onChange={() => {
+              clickToggle();
+            }}
+          />
+          <label
+            htmlFor="checkbox"
+            className="checkbox-label bg-[#111] dark:bg-white"
+          >
+            <svg
+              className="w-[13px] h-[13px]"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 384 512"
+            >
+              <path
+                fill="#f1c40f"
+                d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"
+              />
+            </svg>
+            <svg
+              className="w-[13px] h-[13px]"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                fill="#f39c12"
+                d="M375.7 19.7c-1.5-8-6.9-14.7-14.4-17.8s-16.1-2.2-22.8 2.4L256 61.1 173.5 4.2c-6.7-4.6-15.3-5.5-22.8-2.4s-12.9 9.8-14.4 17.8l-18.1 98.5L19.7 136.3c-8 1.5-14.7 6.9-17.8 14.4s-2.2 16.1 2.4 22.8L61.1 256 4.2 338.5c-4.6 6.7-5.5 15.3-2.4 22.8s9.8 13 17.8 14.4l98.5 18.1 18.1 98.5c1.5 8 6.9 14.7 14.4 17.8s16.1 2.2 22.8-2.4L256 450.9l82.5 56.9c6.7 4.6 15.3 5.5 22.8 2.4s12.9-9.8 14.4-17.8l18.1-98.5 98.5-18.1c8-1.5 14.7-6.9 17.8-14.4s2.2-16.1-2.4-22.8L450.9 256l56.9-82.5c4.6-6.7 5.5-15.3 2.4-22.8s-9.8-12.9-17.8-14.4l-98.5-18.1L375.7 19.7zM269.6 110l65.6-45.2 14.4 78.3c1.8 9.8 9.5 17.5 19.3 19.3l78.3 14.4L402 242.4c-5.7 8.2-5.7 19 0 27.2l45.2 65.6-78.3 14.4c-9.8 1.8-17.5 9.5-19.3 19.3l-14.4 78.3L269.6 402c-8.2-5.7-19-5.7-27.2 0l-65.6 45.2-14.4-78.3c-1.8-9.8-9.5-17.5-19.3-19.3L64.8 335.2 110 269.6c5.7-8.2 5.7-19 0-27.2L64.8 176.8l78.3-14.4c9.8-1.8 17.5-9.5 19.3-19.3l14.4-78.3L242.4 110c8.2 5.7 19 5.7 27.2 0zM256 368a112 112 0 1 0 0-224 112 112 0 1 0 0 224zM192 256a64 64 0 1 1 128 0 64 64 0 1 1 -128 0z"
+              />
+            </svg>
+            <span className="ball bg-white dark:bg-[#111]"></span>
+          </label>
+        </div>
+        <ul className="flex flex-col p-[20px] gap-[20px] text-[14px] font-bold text-[#282828] dark:text-white">
+          <NavLink
+            to="/"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "linear-text" : ""
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "linear-text" : ""
+            }
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="/techstack"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "linear-text" : ""
+            }
+          >
+            Tech Stack
+          </NavLink>
+          <NavLink
+            to="/projects"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "linear-text" : ""
+            }
+          >
+            Projects
+          </NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </ul>
+        <ul className="px-[20px] flex flex-row gap-[20px]">
+          <li>
+            <svg
+              className="w-[20px] h-[20px]"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 496 512"
+            >
+              <path
+                className="dark:fill-[#fff] fill-[#282828]"
+                d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+              />
+            </svg>
+          </li>
+          <li>
+            <svg
+              className="w-[20px] h-[20px]"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                className="dark:fill-[#fff] fill-[#282828]"
+                d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"
+              />
+            </svg>
+          </li>
+          <li>
+            <svg
+              className="w-[20px] h-[20px]"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+            >
+              <path
+                className="dark:fill-[#fff] fill-[#282828]"
+                d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"
+              />
+            </svg>
+          </li>
+        </ul>
       </div>
     </div>
   );
--- src/components/Transition/index.jsx
+++ src/components/Transition/index.jsx
@@ -1,24 +1,24 @@
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 // eslint-disable-next-line react/prop-types
 const Transition = ({ children }) => {
   return (
     <>
       {children}
-      <motion.div
-        className="side-in"
+      {/* <motion.div
+        className="side-in hidden lg:block"
         initial={{ scaleY: 1 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 1 }}
         transition={{ duration: 3, ease: [0.22, 1, 0.36, 1] }}
       />
       <motion.div
-        className="side-out"
+        className="side-out hidden lg:block"
         initial={{ scaleY: 1 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 0 }}
         transition={{ duration: 3, ease: [0.22, 1, 0.36, 1] }}
-      />
+      /> */}
     </>
   );
 };
